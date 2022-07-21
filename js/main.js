fe44d0fa32bbee084ed4ff808c56a18d1f0361ef
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-21 16:38:32 +0200

    Add html&js + vue

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        message: ' Wish You Were Here',
+        image: 'img/wish.jpg'
+    }
+
+})
+
+
+
+console.log('VUE HELLO' + Vue)
